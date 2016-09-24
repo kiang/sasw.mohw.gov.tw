8171a79e12c04db051c1e695e507022257b73f3b
kiang <kiange@gmail.com>
2016-09-24 23:47:36 +0800

    download reports

--- check.php
+++ check.php
@@ -1,12 +1,56 @@
 <?php
 
 $now = time();
+$result = array();
+$dollarKeys = array('預募金額', '實募金額', '社會福利支出', '社會慈善支出', '教育文化支出', '人道救援支出', '其他支出', '必要支出', '總支出');
 foreach(glob(__DIR__ . '/solicitView_detail/*.json') AS $jsonFile) {
   $json = json_decode(file_get_contents($jsonFile), true);
-  $t = strtotime($json['勸募活動結束']);
-  if($t < $now) {
-    $json['預募金額'] = str_replace(array('元', ','), '', $json['預募金額']);
-    $json['實募金額'] = str_replace(array('元', ','), '', $json['實募金額']);
-    echo "{$json['活動名稱']} - " . round($json['實募金額'] / $json['預募金額'], 2) . "\n";
+  print_r($json); exit();
+  foreach($dollarKeys AS $dollarKey) {
+    if(isset($json[$dollarKey])) {
+      $json[$dollarKey] = str_replace(array('元', ','), '', $json[$dollarKey]);
+    }
   }
+  $t = strtotime($json['勸募活動結束']);
+  if(!empty($json['申請單位']) && $t < $now) {
+    $key = $json['申請單位'];
+    if(!isset($result[$key])) {
+      $result[$key] = array(
+        'org' => $key,
+        'avg' => 0,
+        'count' => 0,
+        'total' => 0,
+        'cases' => array(),
+      );
+    }
+    $result[$key]['count'] += 1;
+    $result[$key]['total'] += $json['實募金額'];
+    $case = array();
+    foreach($dollarKeys AS $dollarKey) {
+      if(isset($json[$dollarKey])) {
+        $case[$dollarKey] = $json[$dollarKey];
+      }
+
+    }
+    $result[$key]['cases'][] = array(
+      'title' => $json['活動名稱'],
+      'result' => $case,
+    );
+  }
+}
+
+foreach($result AS $k => $v) {
+  $result[$k]['avg'] = round($result[$k]['total'] / $result[$k]['count']);
+}
+
+usort($result, "cmp");
+
+print_r($result);
+
+function cmp($a, $b)
+{
+    if ($a['avg'] == $b['avg']) {
+        return 0;
+    }
+    return ($a['avg'] > $b['avg']) ? -1 : 1;
 }
--- solicitView.php
+++ solicitView.php
@@ -2,14 +2,26 @@
 
 $cachePath = __DIR__ . '/tmp';
 $detailPath = __DIR__ . '/tmp/detail';
+$scorePath = __DIR__ . '/tmp/score';
+$closePath = __DIR__ . '/tmp/close';
 if(!file_exists($detailPath)) {
   mkdir($detailPath, 0777, true);
 }
-$targetPath = __DIR__ . '/solicitView_detail';
+if(!file_exists($scorePath)) {
+  mkdir($scorePath, 0777, true);
+}
+if(!file_exists($closePath)) {
+  mkdir($closePath, 0777, true);
+}
+$targetPath = __DIR__ . '/detail';
 if(!file_exists($targetPath)) {
   mkdir($targetPath, 0777, true);
 }
-$fh = fopen(__DIR__ . '/solicitView.csv', 'w');
+$reportPath = __DIR__ . '/report';
+if(!file_exists($reportPath)) {
+  mkdir($reportPath, 0777, true);
+}
+$fh = fopen(__DIR__ . '/list.csv', 'w');
 fputcsv($fh, array('ID', '組織', '組織網址', '活動名稱', '活動地區', '開始日期', '結束日期'));
 for($i = 1; $i <= 8; $i++) {
   getPage($i);
@@ -17,7 +29,7 @@ for($i = 1; $i <= 8; $i++) {
 
 
 function getPage($p = 1) {
-  global $cachePath, $fh, $detailPath, $targetPath;
+  global $cachePath, $fh, $detailPath, $scorePath, $closePath, $targetPath, $reportPath;
   $offset = ($p - 1) * 100;
   $pageUrl = 'http://sasw.mohw.gov.tw/app39/solicitView/index?max=100&offset=' . $offset;
   $cacheFile = $cachePath . '/page_' . $p;
@@ -82,5 +94,64 @@ function getPage($p = 1) {
       $data[$detailCols[0]] = $detailCols[1];
     }
     file_put_contents($targetPath . '/' . $id . '.json', json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+    $reportTarget = $reportPath . '/' . $id;
+    if(!empty($data['成果備查報告書'])) {
+      if(!file_exists($reportTarget)) {
+        mkdir($reportTarget, 0777);
+      }
+      $scoreFile = $scorePath . '/' . $id;
+      if(!file_exists($scoreFile) || filesize($scoreFile) === 0) {
+        file_put_contents($scoreFile, file_get_contents($data['成果備查報告書']));
+      }
+      $score = file_get_contents($scoreFile);
+      $lines = explode('</tr>', $score);
+      foreach($lines AS $k => $line) {
+        $pos = strpos($line, 'fileDownload');
+        if(false !== $pos) {
+          $pos += 13;
+          $downloadKey = substr($line, $pos, strpos($line, '\'', $pos) - $pos);
+          $cols = explode('</td>', $line);
+          $parts = explode('<button', $cols[1]);
+          $fileName = trim(strip_tags($parts[0]));
+          $p = pathinfo($fileName);
+          $downloadFileName = $downloadKey . '.' . $p['extension'];
+          $downloadFile = $reportTarget . '/' . $downloadFileName;
+          if(!file_exists($downloadFile)) {
+            file_put_contents($downloadFile, file_get_contents('http://sasw.mohw.gov.tw/app39/download/fileDownload/' . $downloadKey));
+          }
+          $lines[$k] = str_replace('/app39/download/fileDownload/' . $downloadKey, $downloadFileName, $lines[$k]);
+        }
+      }
+      file_put_contents($reportTarget . '/score.html', implode('</tr>', $lines));
+    }
+    if(!empty($data['結案備查報告書'])) {
+      if(!file_exists($reportTarget)) {
+        mkdir($reportTarget, 0777);
+      }
+      $closeFile = $closePath . '/' . $id;
+      if(!file_exists($closeFile) || filesize($closeFile) === 0) {
+        file_put_contents($closeFile, file_get_contents($data['結案備查報告書']));
+      }
+      $close = file_get_contents($closeFile);
+      $lines = explode('</tr>', $close);
+      foreach($lines AS $k => $line) {
+        $pos = strpos($line, 'fileDownload');
+        if(false !== $pos) {
+          $pos += 13;
+          $downloadKey = substr($line, $pos, strpos($line, '\'', $pos) - $pos);
+          $cols = explode('</td>', $line);
+          $parts = explode('<button', $cols[1]);
+          $fileName = trim(strip_tags($parts[0]));
+          $p = pathinfo($fileName);
+          $downloadFileName = $downloadKey . '.' . $p['extension'];
+          $downloadFile = $reportTarget . '/' . $downloadFileName;
+          if(!file_exists($downloadFile)) {
+            file_put_contents($downloadFile, file_get_contents('http://sasw.mohw.gov.tw/app39/download/fileDownload/' . $downloadKey));
+          }
+          $lines[$k] = str_replace('/app39/download/fileDownload/' . $downloadKey, $downloadFileName, $lines[$k]);
+        }
+      }
+      file_put_contents($reportTarget . '/close.html', implode('</tr>', $lines));
+    }
   }
 }
